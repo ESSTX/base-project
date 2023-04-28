3a8c9a136a9c6ec296f522e5e21e2cf01773b8e4
ESSTX <66343496+ESSTX@users.noreply.github.com>
2023-04-28 13:50:56 +0500

    just cool things

--- js/base.js
+++ js/base.js
@@ -1,4 +1,59 @@
-function isMobile() {
+function currentBrowser() {
+  const userAgent = navigator.userAgent.toLowerCase();
+  let browser;
+
+  switch (true) {
+    case /edge/i.test(userAgent):
+      browser = 'Edge';
+      break;
+    case /chrome/i.test(userAgent):
+      browser = 'Chrome';
+      break;
+    case /firefox/i.test(userAgent):
+      browser = 'Firefox';
+      break;
+    case /safari/i.test(userAgent):
+      browser = 'Safari';
+      break;
+    case /opera|opr/i.test(userAgent):
+      browser = 'Opera';
+      break;
+    case /yabrowser/i.test(userAgent):
+      browser = 'Yandex';
+      break;
+    case /vivaldi/i.test(userAgent):
+      browser = 'Vivaldi';
+      break;
+    case /brave/i.test(userAgent):
+      browser = 'Brave';
+      break;
+    case /chromium/i.test(userAgent):
+      browser = 'Chromium';
+      break;
+    case /maxthon/i.test(userAgent):
+      browser = 'Maxthon';
+      break;
+    case /palemoon/i.test(userAgent):
+      browser = 'Pale Moon';
+      break;
+    case /seamonkey/i.test(userAgent):
+      browser = 'SeaMonkey';
+      break;
+    case /ucbrowser/i.test(userAgent):
+      browser = 'UC Browser';
+      break;
+    case /vivaldi/i.test(userAgent):
+      browser = 'Vivaldi';
+      break;
+    default:
+      browser = 'Unknown';
+      break;
+  }
+
+  return browser;
+}
+
+function isNotDesktop() {
   const toMatch = [
     /Android/i,
     /webOS/i,
@@ -7,7 +62,19 @@ function isMobile() {
     /iPod/i,
     /BlackBerry/i,
     /Windows Phone/i,
-    //      /Windows/i
+    /Tablet/i,
+    /Mobile/i,
+    /Opera Mini/i,
+    /IEMobile/i,
+    /Kindle/i,
+    /PlayBook/i,
+    /Nintendo DSi/i,
+    /Nintendo 3DS/i,
+    /Nintendo Switch/i,
+    /PlayStation Vita/i,
+    /PlayStation Portable/i,
+    /Xbox One/i,
+    /Xbox Series X/i,
   ];
 
   return toMatch.some((toMatchItem) => {
@@ -38,7 +105,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  if (isMobile()) {
-    document.querySelector('.error-inside').style.display = "flex";
+  if (isNotDesktop()) {
+    document.querySelector('.warn-device-inside').style.display = "flex";
   }
+  console.log(currentBrowser());
 });
